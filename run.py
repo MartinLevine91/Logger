11412091206b70bac45f4f60997f3a4fee124277
MartinLevine91 <MartinLevine.91@gmail.com>
2013-03-25 11:52:23 +0000

    Create run.py

---
+++ run.py
@@ -0,0 +1,43 @@
+"""
+This module:
+* Keeps track of program state
+* Interprets user inputs, sending some through main.py
+* Sends the state to graphicalOut.py to update the UI
+
+
+The state needs to know what node is currently active, as well the user's
+location within the program.
+
+
+- read in current data
+- offer user options
+
+> Add new log entry
+>> Choose between logs until hit leaf
+>>> Enter data for new entry
+> Add/edit logs
+>> Add log
+>>> Flick through branches until decide where to add leaf
+>>>> Fill in fieds for new log
+>> Edit log
+>>> Pick log to edit
+>>>> Edit location of log in data structure
+>>>> Edit fields for log
+> View log data
+>> Pick log
+
+
+Menu options stored as nodes, their functions must all be able to run with only State as input.
+
+When an option is selected, if it is a leaf then something along the lines of:
+"runMenuOption(currentNode.key, state)"
+is run.
+
+
+
+
+
+"""
+
+
+import main
