376631ec9973e751712a7a5ee7b317b2e57660de
MartinLevine91 <MartinLevine.91@gmail.com>
2013-03-25 11:52:53 +0000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+"""
+This module sets up the data structure required for run.py
+
+"""
+
+import main
+
+menuRoot = main.new_root("menuRoot")
+addEntries = root.branch("addEntries")
+addEntrie_pickLog = addEntries.leaf("pickLogToAddEntriesTo")
