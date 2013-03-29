a7059c44376263c8e16d61798c256f98ca3dde5f
MartinLevine91 <MartinLevine.91@gmail.com>
2013-03-29 17:27:12 +0000

    Update run.py

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ This module:
 * Sends the state to graphicalOut.py to update the UI
 
 
-The state needs to know what node is currently active, as well the user's
+The state needs to know what menu-node is currently active, as well the user's
 location within the program.
 
 
@@ -30,14 +30,79 @@ location within the program.
 Menu options stored as nodes, their functions must all be able to run with only State as input.
 
 When an option is selected, if it is a leaf then something along the lines of:
-"runMenuOption(currentNode.key, state)"
+"runMenuOption(currentNode, state)"
 is run.
 
-
-
+* Build in menu navigation with a runMenuOption simply printing the selected leags' location.
 
 
 """
 
 
 import main
+
+import os
+
+def selectLeaf(current):
+    while True:
+        listOfChildren = current.children()
+        for i in range(len(listOfChildren)):
+            print str(i) + ". " + listOfChildren[i].key()
+        if not isinstance(current,main.Root):
+            print str(len(listOfChildren)) +". Back"
+        else:
+            print str(len(listOfChildren)) +". Quit"
+        
+        userIn = input()
+            
+        if isinstance(userIn,int):
+            if userIn < len(listOfChildren) and userIn >= 0:
+                current = listOfChildren[userIn]
+            elif userIn == len(listOfChildren):
+                current = current.parent()
+
+        if isinstance(current,main.Leaf):
+            returnStr = ""
+            while not isinstance(current,main.Root):
+                returnStr = "/" + current.key() +  returnStr
+                current = current.parent()
+            returnStr = current.key() + returnStr
+            break
+        if current == None:
+            returnStr = "User quit without selecting an option."
+            break
+    print returnStr
+    return current
+
+
+
+dataTypes = {"integer": int,"float": float, "range:": tuple, "choice": main.Root,"timeStamp":
+
+def addLogTemplate(branch):
+"""
+v0
+-add field, give options for dataType
+# number, range, text, choice, timestamp 
+"""
+    
+    while True:
+        break
+
+
+
+
+menu = main.read("Menu.xml")
+current = menu
+while current != None:
+    os.system('clear') 
+    current = selectLeaf(menu)
+    print current
+
+
+
+
+
+
+
+
+
