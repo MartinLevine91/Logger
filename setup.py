650cc2ccce4e6616c284b19a5cc1a9cebe1d12d2
MartinLevine91 <MartinLevine.91@gmail.com>
2013-04-04 03:44:15 +0200

    added a little to the setup

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ current = main.root("root")
 current.leaf("Enter data")
 
 # > Add/edit log templates
-current = current.branch("Add/Edit log templates")
+current = current.branch("Add or Edit log templates")
 
 # >> Add log template
 current.leaf("Add log")
@@ -40,5 +40,13 @@ current.leaf("View log data")
 
 current.write("Menu.xml")
 
+
+logs = root('logs')
+health = logs.branch('Health')
+visit = health.leaf('Toilet Visit')
+visit.field('Solidity', 'range(0,10)', None, False, '0 for completely liquid, 10 for healthy')
+visit.field('Gut pain', 'range(0,10)', None, False, '0 for no pain, 10 for screaming')
+visit.entry({'Solidity': 6, 'Gut pain': 3})
+visit.entry({'Solidity': 3})
 
 
