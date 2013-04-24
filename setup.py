b9fe337f6f39f1fd6f46f6e64dee99adf69b89d9
MartinLevine91 <MartinLevine.91@gmail.com>
2013-04-24 13:43:44 +0200

    Setup.py now has a reduced function
    Due to changes in program structure, it is no longer necessary to setup a menu Node tree, so setup.py now only builds an example log Node tree.

--- setup.py
+++ setup.py
@@ -1,48 +1,8 @@
 """
-This module sets up the data structure required for run.py
-
-> Add new log entry
-> Add/edit log template
->> Add log template
->> Edit log template
->>> Edit location of log in data structure
->>> Edit fields for log
-> View log data
-
-
+This module creates an example LogTree called "Logs.xml"
 """
 
 import main
-
-
-
-
-
-# Create new tree
-current = main.root("root")
-
-# > Add a new log entry
-current.leaf("Enter data")
-
-# > Add/edit log templates
-current = current.branch("Add or Edit log templates")
-
-# >> Add log template
-current.leaf("Add log")
-
-# >> Edit log templates
-current = current.branch("Edit log")
-
-# >>> Edit the location of a log template in log-tree
-current.leaf("Edit location of a log template in log-tree")
-# >>> Edit the fields for log
-current.leaf("Edit the fields in a log template")
-current = current.parent()
-current = current.parent()
-# > View log data
-current.leaf("View log data")
-
-current.write("Menu.xml")
 
 
 logs = main.root('logs')
@@ -53,5 +13,3 @@ visit.field('Gut pain', 'range(0,10)', None, False, '0 for no pain, 10 for screa
 visit.entry({'Solidity': 6, 'Gut pain': 3})
 visit.entry({'Solidity': 3})
 logs.write("Logs.xml")
-
-
