3bef4d880ccb309e375feb82d7d1e3f668d2880a
MartinLevine91 <MartinLevine.91@gmail.com>
2013-05-10 18:14:46 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,11 +8,10 @@ import main
 logs = main.root('logs')
 health = logs.branch('Health')
 visit = health.leaf('Toilet Visit')
-visit.field('Solidity', 'range(0,10)', None, False, '0 for completely liquid, 10 for healthy')
-visit.field('Gut pain', 'range(0,10)', None, False, '0 for no pain, 10 for screaming')
+visit.field('Solidity', '["Int",[]]', None, False, '0 for completely liquid, 5 for healthy, 10 for rocks',False)
+visit.field('Gut pain', '["Int",[]]', None, False, '0 for no pain, 10 for screaming',False)
 visit.entry({'Solidity': 6, 'Gut pain': 3})
 visit.entry({'Solidity': 3})
 logs.write("Logs.xml")
-
 
 
