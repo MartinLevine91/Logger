1525d2f57ace458922173a22ee07cce741f75e03
MartinLevine91 <MartinLevine.91@gmail.com>
2013-05-23 18:25:28 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,14 @@
 """
 This module creates an example LogTree called "Logs.xml"
 """
-
 import main
 
 
 logs = main.root('logs')
 health = logs.branch('Health')
 visit = health.leaf('Toilet Visit')
-visit.field('Solidity', '["Int",[]]', None, False, '0 for completely liquid, 5 for healthy, 10 for rocks',False)
-visit.field('Gut pain', '["Int",[]]', None, False, '0 for no pain, 10 for screaming',False)
+visit.field('Solidity', '["Int",[]]', 2, False, '0 for completely liquid, 5 for healthy, 10 for rocks.',False)
+visit.field('Gut pain', '["Int",[]]', None, True, '0 for no pain, 10 for screaming',False)
 visit.entry({'Solidity': 6, 'Gut pain': 3})
 visit.entry({'Solidity': 3})
 logs.write("Logs.xml")
